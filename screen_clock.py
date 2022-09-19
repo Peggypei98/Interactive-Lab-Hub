fdc560e892efeedfa247efda60df152e82030179
Peggypei98 <zpeggy0426@gmail.com>
2022-09-19 19:08:39 -0400

    update with images

--- screen_clock.py
+++ screen_clock.py
@@ -5,6 +5,11 @@ import board
 from PIL import Image, ImageDraw, ImageFont
 from time import strftime, sleep
 import adafruit_rgb_display.st7789 as st7789
+import adafruit_rgb_display.hx8357 as hx8357  # pylint: disable=unused-import
+import adafruit_rgb_display.st7735 as st7735  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1351 as ssd1351  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1331 as ssd1331  # pylint: disable=unused-import
+import adafruit_rgb_display.ili9341 as ili9341
 
 # Get UV index: https://github.com/bachya/pyopenuv/
 
@@ -89,6 +94,165 @@ sunset_m = int(today_ss.strftime("%M"))
 print('Today at Cornell Tech the sun raised at {} and get down at {} UTC'.
       format(today_sr.strftime('%H:%M'), today_ss.strftime('%H:%M')))
 
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+# Set buttom
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+x1 = 0
+x2 = 0
+y1=5
+y2=30
+
+def sun():
+    # Configuration for CS and DC pins (these are PiTFT defaults):
+    cs_pin = digitalio.DigitalInOut(board.CE0)
+    dc_pin = digitalio.DigitalInOut(board.D25)
+    reset_pin = digitalio.DigitalInOut(board.D24)
+
+    BAUDRATE = 24000000
+
+    spi = board.SPI()
+
+
+    disp = st7789.ST7789(
+        spi,
+        cs=cs_pin,
+        dc=dc_pin,
+        rst=reset_pin,
+        baudrate=BAUDRATE,
+        width=135,
+        height=240,
+        x_offset=53,
+        y_offset=40,
+    )
+    # pylint: enable=line-too-long
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode 'RGB' for full color.
+    if disp.rotation % 180 == 90:
+        height = disp.width  # we swap height/width to rotate it to landscape!
+        width = disp.height
+    else:
+        width = disp.width  # we swap height/width to rotate it to landscape!
+        height = disp.height
+    image = Image.new("RGB", (width, height))
+
+
+    import PIL.Image
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+    image1 = Image.open("sunrise.jpg")
+    image = image1.convert('RGB')
+
+    backlight = digitalio.DigitalInOut(board.D22)
+    backlight.switch_to_output()
+    backlight.value = True
+
+
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    # Display image.
+    disp.image(image)
+
+def moon():
+    # Configuration for CS and DC pins (these are PiTFT defaults):
+    cs_pin = digitalio.DigitalInOut(board.CE0)
+    dc_pin = digitalio.DigitalInOut(board.D25)
+    reset_pin = digitalio.DigitalInOut(board.D24)
+
+    BAUDRATE = 24000000
+
+    spi = board.SPI()
+
+
+    disp = st7789.ST7789(
+        spi,
+        cs=cs_pin,
+        dc=dc_pin,
+        rst=reset_pin,
+        baudrate=BAUDRATE,
+        width=135,
+        height=240,
+        x_offset=53,
+        y_offset=40,
+    )
+    # pylint: enable=line-too-long
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode 'RGB' for full color.
+    if disp.rotation % 180 == 90:
+        height = disp.width  # we swap height/width to rotate it to landscape!
+        width = disp.height
+    else:
+        width = disp.width  # we swap height/width to rotate it to landscape!
+        height = disp.height
+    image = Image.new("RGB", (width, height))
+
+
+    import PIL.Image
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+    image1 = Image.open("sunset.jpg")
+    image = image1.convert('RGB')
+
+    backlight = digitalio.DigitalInOut(board.D22)
+    backlight.switch_to_output()
+    backlight.value = True
+
+
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    # Display image.
+    disp.image(image)
+
 # On a special date in your machine's local time zone
 #abd = datetime.date(2014, 10, 3)
 #abd_sr = sun.get_local_sunrise_time(abd)
@@ -98,15 +262,16 @@ print('Today at Cornell Tech the sun raised at {} and get down at {} UTC'.
 
 # strftime: https://www.geeksforgeeks.org/python-strftime-function/#:~:text=The%20strftime()%20function%20is,and%20returns%20the%20string%20representation.&text=Returns%20%3A%20It%20returns%20the%20string,the%20date%20or%20time%20object.
 
-image1 = Image.open("sunrise.jpg")
-scaled_width = scaled_height = image1.width // 300
-image1 = image1.resize((scaled_width, scaled_height), Image.BICUBIC)
-disp.image(image1)
+#image1 = Image.open("sunrise.jpg")
+#scaled_width = scaled_height = image1.width // 300
+#image1 = image1.resize((scaled_width, scaled_height), Image.BICUBIC)
+#disp.image(image1)
 
-image2 = Image.open("sunset.jpg")
-scaled_width = scaled_height = image2.width // 150
-image2 = image2.resize((scaled_width, scaled_height), Image.BICUBIC)
-disp.image(image2)
+#image2 = Image.open("sunset.jpg")
+#scaled_width = scaled_height = image2.width // 150
+#image2 = image2.resize((scaled_width, scaled_height), Image.BICUBIC)
+#disp.image(image2)
+
 
 
 
@@ -148,17 +313,22 @@ while True:
     y += font.getsize(sunrise)[1]
     draw.text((175, 110), sunset, font=fontSunt, fill="#f57c2c")
 
+    if not buttonA.value:
+        sun()
+    if not buttonB.value:
+        moon()
+
     
     # Display image.
-    disp.image(image1, rotation)
-    disp.image(image2, rotation)
+    #disp.image(image1, rotation)
+    #disp.image(image2, rotation)
     #if nowhours in range(5,17):
         #disp.image(image1,rotation)
     #else:
         # disp.image(image2,rotation)
 
  # Display image.
-    #disp.image(image, rotation)
+    disp.image(image, rotation)
     time.sleep(0.1)
 
 
